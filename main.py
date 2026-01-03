ccec2e78af8c6745654994e30a70f1067a43d37a
bnether <ben.netherclift@gmail.com>
2026-01-03 16:35:44 +0000

    adding another example

--- main.py
+++ main.py
@@ -568,9 +568,9 @@ elif st.session_state.page == "Examples":
     )
 
     render_example_case(
-        title="Circular Shim",
-        prompt="40mm tall circular shim, outer diameter 36mm, inner diameter 30mm",
-        stl_path="static/example_shim.stl" # Ensure these files exist in your static folder
+        title="Mounting Plate",
+        prompt="10mm thick plate, 100mm wide and 150mm long. An M6 counterbored hole in each corner, 12mm from each edge.",
+        stl_path="static/example_plate.stl" # Ensure these files exist in your static folder
     )
 
     render_example_case(
@@ -800,6 +800,7 @@ st.markdown("""
         <p style="font-size:0.75rem; margin-top: 25px; opacity: 0.7; color: white;">© 2025 Napkin Manufacturing Tool. All rights reserved.</p>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
